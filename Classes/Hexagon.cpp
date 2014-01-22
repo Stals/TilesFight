adbe9730c404b2f6b2ca77569d8dba582a548763
Stals <superstals@gmail.com>
2014-01-22 23:02:34 +0400

    trying more frequent generation

--- Classes/Hexagon.cpp
+++ Classes/Hexagon.cpp
@@ -121,7 +121,7 @@ void Hexagon::runScaleAction()
 
 void Hexagon::runScaleLabelAction()
 {
-	CCEaseInOut* action1 = CCEaseInOut::create(CCScaleTo::create(0.1f, 1.2f, 1.2f), 2.f);
+	CCEaseInOut* action1 = CCEaseInOut::create(CCScaleTo::create(0.1f, 1.1f, 1.1f), 2.f);
 	CCEaseInOut* action2 = CCEaseInOut::create(CCScaleTo::create(0.5f, 1.f, 1.f), 2.f);
 	CCSequence* seq = CCSequence::create(action1, action2, NULL);
 
