59d76b1545ccecadf4f2d19c2cfe24473ed84eec
Stanislav Korotaev <superstals@gmail.com>
2014-01-26 18:19:53 +0400

    randomGenerator improvements

--- Classes/utils/RandomGenerator.h
+++ Classes/utils/RandomGenerator.h
@@ -1,11 +1,15 @@
 #pragma once
+#include <vector>
 
 class RandomGenerator{
 public:
-	// не включая max!
+	// РЅРµ РІРєР»СЋС‡Р°СЏ max!
 	static int getRandom(int min, int max);
 	static double getRandomDouble(double min, double max);
 	static double getRandom01();
+    
+    static void normalize(std::vector<double> &weights);
+    static size_t getRandomID(std::vector<double> weights);
 
 private:
 	RandomGenerator();
