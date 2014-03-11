23fade5c4990c30631c4972ee4fadffd29345a6b
Stanislav Korotaev <superstals@gmail.com>
2014-03-11 23:21:50 +0400

    basic vs ai screen

--- Classes/TitleScreens/VsAIScreen.cpp
+++ Classes/TitleScreens/VsAIScreen.cpp
@@ -8,8 +8,57 @@
 
 #include "VsAIScreen.h"
 #include "../ColorPicker.h"
+#include "../ButtonFactory.h"
+#include "Constants.h"
+#include "../Game.h"
+
+#define FONT_SIZE 24.f
+#define PADDING -5.f
+#define BUTTON_TEXT "GO!"
+
 
 VsAIScreen::VsAIScreen()
 {
-    this->addChild(new ColorPicker);
+    setupColorPicker();
+    setupLabels();
+    setupButton();
+}
+
+void VsAIScreen::setupColorPicker()
+{
+    picker = new ColorPicker;
+    
+    this->addChild(picker);
+    
+    picker->setPositionX(-this->getContentSize().width/4);
+    
+    picker->pickAtIndex(0);
+
+}
+
+void VsAIScreen::setupLabels()
+{
+    CCLabelTTF* label1 = CCLabelTTF::create("PLAYER", default_font.c_str(), FONT_SIZE);
+    CCLabelTTF* label2 = CCLabelTTF::create("AI", default_font.c_str(), FONT_SIZE);
+    
+    this->addChild(label1);
+    this->addChild(label2);
+    
+    label1->setPosition(ccp(-this->getContentSize().width/4,
+                           picker->getContentSize().height/2 + PADDING));
+    
+    label2->setPosition(ccp(this->getContentSize().width/4,
+                            picker->getContentSize().height/2 + PADDING));
+}
+
+void VsAIScreen::setupButton()
+{
+    Button* button = ButtonFactory::button(BUTTON_TEXT, new Handler(this, menu_selector(VsAIScreen::startGame)), -261);
+    
+    this->addChild(button);
+}
+
+void VsAIScreen::startGame(CCObject* pSender)
+{
+    Game::current().starNewGame(Game::VS_AI);
 }
--- Classes/TitleScreens/VsAIScreen.h
+++ Classes/TitleScreens/VsAIScreen.h
@@ -2,8 +2,18 @@
 
 #include "EmptyScreen.h"
 
+class ColorPicker;
+
 class VsAIScreen : public EmptyScreen{
 public:
     VsAIScreen();
 
+private:
+    void setupColorPicker();
+    void setupLabels();
+    void setupButton();
+    
+    ColorPicker* picker;
+    
+    void startGame(cocos2d::CCObject* pSender);
 };
