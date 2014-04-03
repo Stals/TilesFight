adefd4a718ae96a3916701b7ef126139d2d29898
Stanislav Korotaev <superstals@gmail.com>
2014-04-03 21:38:54 +0400

    added glow to ai buttons

--- Classes/AIButton.cpp
+++ Classes/AIButton.cpp
@@ -18,17 +18,31 @@ CCSprite* AIButton::getInactive()
 {
     CCSprite* sprite = CCSprite::create();
     
+    CCSprite* glow = CCSprite::create(IMG("glow_dark.png"));
+    
+    sprite->addChild(glow);
+    CCSize contentSize = glow->getContentSize();
+    sprite->setContentSize(contentSize);
+
+    
     cocos2d::CCLabelTTF* rankLabel = createRank(rank);
     cocos2d::CCLabelTTF* nameLabel = createName(name);
     cocos2d::CCLabelTTF* difficultyLabel = createDifficulty(difficulty);
     
-    rankLabel->setPositionY(PADDING);
-    difficultyLabel->setPositionY(-PADDING);
+    glow->setPositionY(contentSize.height/2 + 10);
+    rankLabel->setPositionY(contentSize.height/2 + PADDING);
+    nameLabel->setPositionY(contentSize.height/2);
+    difficultyLabel->setPositionY(contentSize.height/2 -PADDING);
+    
+    glow->setPositionX(contentSize.width/2);
+    rankLabel->setPositionX(contentSize.width/2);
+    nameLabel->setPositionX(contentSize.width/2);
+    difficultyLabel->setPositionX(contentSize.width/2);
     
     sprite->addChild(rankLabel);
     sprite->addChild(nameLabel);
     sprite->addChild(difficultyLabel);
-    
+
     return sprite;
 }
 
@@ -36,12 +50,26 @@ CCSprite* AIButton::getActive()
 {
     CCSprite* sprite = CCSprite::create();
     
+    CCSprite* glow = CCSprite::create(IMG("glow.png"));
+    
+    sprite->addChild(glow);
+    CCSize contentSize = glow->getContentSize();
+    sprite->setContentSize(contentSize);
+    
+    
     cocos2d::CCLabelTTF* rankLabel = createRank(rank);
     cocos2d::CCLabelTTF* nameLabel = createName(name);
     cocos2d::CCLabelTTF* difficultyLabel = createDifficulty(difficulty);
     
-    rankLabel->setPositionY(PADDING);
-    difficultyLabel->setPositionY(-PADDING);
+    glow->setPositionY(contentSize.height/2 + 10);
+    rankLabel->setPositionY(contentSize.height/2 + PADDING);
+    nameLabel->setPositionY(contentSize.height/2);
+    difficultyLabel->setPositionY(contentSize.height/2 -PADDING);
+    
+    glow->setPositionX(contentSize.width/2);
+    rankLabel->setPositionX(contentSize.width/2);
+    nameLabel->setPositionX(contentSize.width/2);
+    difficultyLabel->setPositionX(contentSize.width/2);
     
     sprite->addChild(rankLabel);
     sprite->addChild(nameLabel);
